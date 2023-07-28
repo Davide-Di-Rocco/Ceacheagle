642afcc86119e201f70b867e36ef71301326b4ca
Matteo Tatoni <83955605+TatoniMatteo@users.noreply.github.com>
2023-07-28 18:57:46 +0200

    update

---
+++ CachEagle/src/environments/environment.ts
@@ -0,0 +1,14 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    mapsKey: "AIzaSyDMPf2TPUkp3TPy1eSZDJeSvQVuYNYgLKE",
+    latAquila: 42.350,
+    lngAquila: 13.399,
+    defaultZoom: 12,
+    hostname: 'http://192.168.178.142:3000/',
+    userDir: 'users',
+    cacheDir: 'caches'
+};
